550c55dd91c753e948e925217d57065946758242
alexthezhang228 <110424337+alexthezhang228@users.noreply.github.com>
2023-03-10 22:02:49 +0100

    adjust css styles

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 '''
 Author: alexthezhang228 zhangy32@tcd.ie
 Date: 2023-02-19 11:18:16
-LastEditors: alexthezhang228 zhangy32@tcd.ie
-LastEditTime: 2023-03-01 23:51:47
+LastEditors: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
+LastEditTime: 2023-03-10 21:10:38
 FilePath: /flask_project/app.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -17,6 +17,8 @@ from flask_migrate import Migrate
 
 app = Flask(__name__, template_folder='template')
 app.config.from_object(config)
+
+
 db.init_app(app)
 mail.init_app(app)
 migrate = Migrate(app, db)
@@ -31,7 +33,6 @@ app.register_blueprint(bp_question)
 @app.before_request
 def my_before_request():
     user_id = session.get('user_id')
-    print(user_id)
     if user_id:
         user = db.session.get(UserModel, user_id)
         setattr(g, 'user', user)
--- blueprints/author.py
+++ blueprints/author.py
@@ -1,8 +1,8 @@
 '''
 Author: alexthezhang228 zhangy32@tcd.ie
 Date: 2023-02-19 12:16:37
-LastEditors: alexthezhang228 zhangy32@tcd.ie
-LastEditTime: 2023-03-01 23:47:56
+LastEditors: alexthezhang228 110424337+alexthezhang228@users.noreply.github.com
+LastEditTime: 2023-03-10 21:25:10
 FilePath: /flask_project/blueprints/author.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -23,7 +23,6 @@ def login():
         return render_template('login.html')
     else:
         form = LoginForm(request.form)
-        # print('form!')
         if form.validate():
             email = form.email.data
             password = form.password.data
